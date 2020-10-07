46235ad6af63df8b9a18c30f13370524301cf63a
Pragya Saxena <saxenapragya@outlook.com>
2020-10-07 17:52:39 +0530

    added loop file demo

---
+++ javabasics/src/LoopsExample.java
@@ -0,0 +1,15 @@
+public class LoopsExample {
+    static {
+        System.out.println("Hi there");
+    }
+    public static void main(String[] args) {
+        int i=0;
+        for(;i<5;)
+        {
+            i++;
+            System.out.println("Numbers: " + i);
+        }
+
+    }
+
+}
